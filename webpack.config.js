ed5650fc27e92d63866b7bbd51fd09a8050355bb
Marta Welfe-Starzak <mwelfe@gmail.com>
2022-11-24 13:23:29 +0100

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,42 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+    optimization: {
+
+        runtimeChunk: 'single',
+
+    },
 
     module: {
-
         rules: [
-
             {
-
                 test: /\.css$/i,
-
                 use: ['style-loader', 'css-loader'],
-
             },
             {
-
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
                 type: 'asset/resource',
-
             },
 
             {
-
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-
                 type: 'asset/resource',
-
             },
 
         ],
