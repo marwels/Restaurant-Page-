71cf8860a47b6dede18f135cba65e0f1a665f4b7
marwels <mwelfe@gmail.com>
2022-12-13 14:39:58 +0100

    feat: finish styling tiles in seet and savory

--- src/contact.js
+++ src/contact.js
@@ -1,34 +1,6 @@
-import { Input } from './Components/Input';
-import { TextArea } from './Components/TextArea';
-import { HumbleDiv } from './Components/HumbleDiv';
-
-const Form = function Form(targetEl) {
-    const form = document.createElement("form");
-    form.setAttribute("method", "post");
-    form.setAttribute("action", "example.com/path");
-
-    const children = [];
-    children.push(Input(form, "formInput", "Email", "email", "email", ""));
-    children.push(Input(form, "formInput", "Name", "text", "name", ""));
-    children.push(Input(form, "formInput", "Address", "text", "address", ""));
-    children.push(TextArea(form, "message", "Message", "message", "6", "" ));
-   
-
-    // create a submit button
-    const submit = document.createElement("button");
-    submit.setAttribute("type", "submit");
-    submit.setAttribute("value", "Submit");
-    submit.innerText = "Submit";
-    form.appendChild(submit);
-
-    targetEl.appendChild(form);
+import { Form } from './Components/Form';
 
 
-    return () => {
-        children.forEach(destroy => destroy());
-        targetEl.removeChild(form);
-    }
-}
 
 const SquareDivWithIcon = function SquareDivWithIcon(parentEl, className, iconUniCode, title, moreText) {
     const divSquareDivWithIcon = document.createElement("div");
